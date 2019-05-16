ec1e20132230139d7877386d47307ffae6a4865f
tom-gough <tom@tgsd.co.uk>
2019-05-16 08:50:25 +0100

    Added logging

--- src/PensionsRegulatorApi/Program.cs
+++ src/PensionsRegulatorApi/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using NLog.Web;
 
 namespace PensionsRegulatorApi
 {
@@ -7,7 +9,21 @@ namespace PensionsRegulatorApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Info("Starting up host");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
