9ca2aba0723b7b3ab73e69fa0d25a86eebfdea57
Neil Winstanley <Winsto@users.noreply.github.com>
2019-05-17 12:59:54 +0100

    AML-3593: make a start on some tests. Note first test passes with default implementation of handler.

--- src/PensionsRegulatorApi/Application/Queries/GetOrganisationsHandler.cs
+++ src/PensionsRegulatorApi/Application/Queries/GetOrganisationsHandler.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using MediatR;
+using PensionsRegulatorApi.Data;
 using PensionsRegulatorApi.Domain;
 
 namespace PensionsRegulatorApi.Application.Queries
 {
+    
+
     public class GetOrganisationsHandler : RequestHandler<GetOrganisations, IEnumerable<Organisation>>
     {
+        public GetOrganisationsHandler(OrganisationRepository repository)
+        {
+
+        }
+        
         protected override IEnumerable<Organisation> Handle(GetOrganisations request)
         {
             return Enumerable.Empty<Organisation>();
---
+++ src/SFA.DAS.PensionsRegulatorApi.UnitTests/Application/Queries/GetOrganisations/Given_Repository_Throws_An_Exception.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using FluentAssertions;
+using MediatR;
+using PensionsRegulatorApi.Application.Queries;
+using PensionsRegulatorApi.Data;
+using PensionsRegulatorApi.Domain;
+
+namespace SFA.DAS.PensionsRegulatorApi.UnitTests.Application.Queries.GetOrganisations
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class GivenL_Repository_Throws_An_Exception
+    {
+        private OrganisationRepository _mockRepository;
+        private IRequestHandler<global::PensionsRegulatorApi.Application.Queries.GetOrganisations, IEnumerable<Organisation>> _sut;
+
+        [SetUp]
+        public void Given()
+        {
+            _mockRepository = Substitute.For<OrganisationRepository>();
+
+            _mockRepository
+                .GetOrganisationsForPAYEReference(String.Empty)
+                .ReturnsForAnyArgs(m => throw new Exception());
+
+            _sut = new GetOrganisationsHandler(_mockRepository);
+        }
+
+        [Test]
+        public async Task Then_Empty_Collection_Is_Returned()
+        {
+            var organisations =
+                await 
+                _sut
+                    .Handle(
+                        new global::PensionsRegulatorApi.Application.Queries.GetOrganisations("anynonemptytext"),
+                        CancellationToken.None);
+
+            organisations
+                .Should()
+                .NotBeNull();
+
+            CollectionAssert
+                .IsEmpty(
+                    organisations);
+        }
+    }
+}
