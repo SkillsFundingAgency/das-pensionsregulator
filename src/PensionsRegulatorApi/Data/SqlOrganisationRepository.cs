0f1fba861487235bbe8dd31237bbd4df9f59501d
Neil Winstanley <Winsto@users.noreply.github.com>
2019-05-17 11:19:37 +0100

    AML-3595: flesh out SQL repository; still best guesses on columns and types for now.

---
+++ src/PensionsRegulatorApi/Data/SqlOrganisationRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using PensionsRegulatorApi.Domain;
+
+namespace PensionsRegulatorApi.Data
+{
+    public class SqlOrganisationRepository : OrganisationRepository
+    {
+        private readonly string _connectionString;
+
+        public SqlOrganisationRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Organisation> GetOrganisationsForPAYEReference(string payeReference)
+        {
+            var retrievedOrganisations = new List<Organisation>(0);
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand()
+                {
+                    CommandText = @"[dbo].GetOrganisationsByPAYEReference",
+                    CommandType = CommandType.StoredProcedure,
+                    Connection = connection,
+                    Parameters = { new SqlParameter{ParameterName = "@PAYESchemeReference", Value = payeReference } }
+                })
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                          retrievedOrganisations
+                              .Add(
+                                  new Organisation
+                                  {
+                                      Name = reader["Name"].ToString(),
+                                      Address = 
+                                          new Address
+                                          {
+                                              Line1 = reader["AddressLine1"].ToString(),
+                                              Line2 = reader["AddressLine2"].ToString(),
+                                              Line3 = reader["AddressLine3"].ToString(),
+                                              Line4 = reader["AddressLine4"].ToString(),
+                                              Line5 = reader["AddressLine5"].ToString(),
+                                              Postcode = reader["PostCode"].ToString()
+                                          }
+                                  });
+                        }
+                    }
+                }
+            }
+
+            return retrievedOrganisations.AsReadOnly();
+        }
+    }
+}
