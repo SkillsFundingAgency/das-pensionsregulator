8b9d4129e711717fb9088aba710afaae0b58fa06
Neil Winstanley <Winsto@users.noreply.github.com>
2019-05-21 12:14:26 +0100

    AML-3593: make capitaliation consistent as per review comments.

--- src/SFA.DAS.PensionsRegulator.Database/StoredProcedures/GetOrganisationsByPAYEReference.sql
+++ src/SFA.DAS.PensionsRegulator.Database/StoredProcedures/GetOrganisationsByPAYEReference.sql
@@ -2,8 +2,8 @@
 	@PAYESchemeReference nvarchar(50)
 AS
 SELECT 
-organisation.OranisationName as Name,
-organisation.Status as Status,
+Organisation.OranisationName as Name,
+Organisation.Status as Status,
 Organisation.TPR_Unique_Id as UniqueIdentity,
 OrganisationAddress.AddressLine1 as AddressLine1,
 OrganisationAddress.AddressLine2 as AddressLine2,
@@ -12,7 +12,7 @@ OrganisationAddress.AddressLine4 as AddressLine4,
 OrganisationAddress.AddressLine5 as AddresssLine5,
 OrganisationAddress.PostCode as PostCode
 FROM
-OrganisationPAYEScheme join
-Organisation on Organisation.Employer_SK = OrganisationPAYEScheme.Employer_SK join
+OrganisationPAYEScheme JOIN
+Organisation on Organisation.Employer_SK = OrganisationPAYEScheme.Employer_SK JOIN
 OrganisationAddress on OrganisationAddress.Employer_SK = OrganisationPAYEScheme.Employer_SK 
 WHERE OrganisationPAYEScheme.PAYESchemeRef = @PAYESchemeReference
