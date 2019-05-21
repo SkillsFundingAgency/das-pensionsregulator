eadd14cf14c50848ed299cd6ebc1b603538b95b9
Neil Winstanley <Winsto@users.noreply.github.com>
2019-05-21 10:39:57 +0100

    AML-3593: Should be using Id not surrogate key after all, doh.

--- src/SFA.DAS.PensionsRegulator.Database/StoredProcedures/GetOrganisationsByPAYEReference.sql
+++ src/SFA.DAS.PensionsRegulator.Database/StoredProcedures/GetOrganisationsByPAYEReference.sql
@@ -4,7 +4,7 @@ AS
 SELECT 
 organisation.OranisationName as Name,
 organisation.Status as Status,
-Organisation.Employer_SK as UniqueIdentity,
+Organisation.Id as UniqueIdentity,
 OrganisationAddress.AddressLine1 as AddressLine1,
 OrganisationAddress.AddressLine2 as AddressLine2,
 OrganisationAddress.AddressLine3 as AddressLine3,
@@ -13,6 +13,6 @@ OrganisationAddress.AddressLine5 as AddresssLine5,
 OrganisationAddress.PostCode as PostCode
 FROM
 OrganisationPAYEScheme join
-Organisation on Organisation.Employer_SK = OrganisationPAYEScheme.Employer_SK join
-OrganisationAddress on OrganisationAddress.Employer_SK = OrganisationPAYEScheme.Employer_SK 
+Organisation on Organisation.Id = OrganisationPAYEScheme.Id join
+OrganisationAddress on OrganisationAddress.Id = OrganisationPAYEScheme.Id 
 WHERE OrganisationPAYEScheme.PAYESchemeRef = @PAYESchemeReference
