3fe304ab39f553f6be7fe7d5ea6c8d69669ce56e
tom-gough <tom@tgsd.co.uk>
2019-05-22 10:11:37 +0100

    Tidy up usings in controller

--- src/PensionsRegulatorApi/Controllers/PensionsRegulatorController.cs
+++ src/PensionsRegulatorApi/Controllers/PensionsRegulatorController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PensionsRegulatorApi.Application.Queries;
 using PensionsRegulatorApi.Domain;
