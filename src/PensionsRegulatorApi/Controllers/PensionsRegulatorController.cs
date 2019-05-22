1e680d8cb2f737c1475bc7316d71988ecaec8006
tom-gough <tom@tgsd.co.uk>
2019-05-22 10:15:52 +0100

    Added xml comments for swagger

--- src/PensionsRegulatorApi/Controllers/PensionsRegulatorController.cs
+++ src/PensionsRegulatorApi/Controllers/PensionsRegulatorController.cs
@@ -12,13 +12,24 @@ namespace PensionsRegulatorApi.Controllers
     [ApiController]
     public class PensionsRegulatorController : ControllerBase
     {
-        private IRequestHandler<GetOrganisations, IEnumerable<Organisation>> _getOrganisationsHandler;
+        private readonly IRequestHandler<GetOrganisations, IEnumerable<Organisation>> _getOrganisationsHandler;
 
         public PensionsRegulatorController(IRequestHandler<GetOrganisations, IEnumerable<Organisation>> getOrganisationsHandler)
         {
             _getOrganisationsHandler = getOrganisationsHandler;
         }
 
+        /// <summary>
+        /// Gets the organisations from the pensions regulator for a given PAYE reference
+        /// </summary>
+        /// <param name="payeRef">The PAYE reference from which to get matching organisations from the pensions regulator</param>
+        /// <returns>The organisations for the given PAYE reference from the pensions regulator</returns>
+        /// <response code="200">Health check successful</response>
+        /// <response code="401">The client is not authorized to access this endpoint</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(200)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(500)]
         [HttpGet("{payeRef}", Name = "Get")]
         public async Task<IEnumerable<Organisation>> Get(string payeRef)
         {
