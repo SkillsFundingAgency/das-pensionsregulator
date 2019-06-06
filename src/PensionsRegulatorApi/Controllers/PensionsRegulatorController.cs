5777f7061d5e347d6a7c993f9ae676af73e771f3
tom-gough <tom@tgsd.co.uk>
2019-06-06 10:11:32 +0100

    Tidied up usings

--- src/PensionsRegulatorApi/Controllers/PensionsRegulatorController.cs
+++ src/PensionsRegulatorApi/Controllers/PensionsRegulatorController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Logging;
 using PensionsRegulatorApi.Application.Queries;
 using PensionsRegulatorApi.Domain;
