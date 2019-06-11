0f33e0db80fc1b8c40ed210c9dd3bf3852ebfb53
Neil Winstanley <Winsto@users.noreply.github.com>
2019-06-11 16:09:52 +0100

    AML-3692: stored procedure for lookup by PAYE reference and AORN

---
+++ src/SFA.DAS.PensionsRegulator.Database/StoredProcedures/GetOrganisationsByPAYEReferenceAndAORN.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].GetOrganisationsByPAYEReferenceAndAORN
+	@PAYESchemeReference varchar(25),
+	@AORN varchar(25)
+AS
+SELECT 
+Organisation.OrganisationName as Name,
+Organisation.Status as Status,
+Organisation.TPR_Unique_Id as UniqueIdentity,
+OrganisationAddress.AddressLine1 as AddressLine1,
+OrganisationAddress.AddressLine2 as AddressLine2,
+OrganisationAddress.AddressLine3 as AddressLine3,
+OrganisationAddress.AddressLine4 as AddressLine4,
+OrganisationAddress.AddressLine5 as AddressLine5,
+OrganisationAddress.PostCode as PostCode
+FROM
+Organisation JOIN
+OrganisationPAYEScheme on OrganisationPAYEScheme.Employer_SK = Organisation.Employer_SK LEFT JOIN
+OrganisationAddress on OrganisationAddress.Employer_SK = OrganisationPAYEScheme.Employer_SK 
+WHERE OrganisationPAYEScheme.PAYESchemeRef = @PAYESchemeReference
+AND       Organisation.AOReference = @AORN
