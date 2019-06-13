737e0fb6988813c515d31a748fc11a0eb7406f5e
Neil Winstanley <Winsto@users.noreply.github.com>
2019-06-13 11:45:20 +0100

    AML-3692: assert that different controller actions behave differently.

---
+++ src/SFA.DAS.PensionsRegulatorApi.UnitTests/Controllers/PayeAndAorn/Given_A_PensionsRegulatorController/Given_A_PensionsRegulatorController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Bson;
+using NSubstitute;
+using NUnit.Framework;
+using PensionsRegulatorApi.Application.Queries;
+using PensionsRegulatorApi.Controllers;
+
+namespace SFA.DAS.PensionsRegulatorApi.UnitTests.Controllers.PayeAndAorn.Given_A_PensionsRegulatorController
+{
+    [ExcludeFromCodeCoverage]
+    public class Given_A_PensionsRegulatorController
+    {
+        protected PensionsRegulatorController SUT;
+        protected IMediator MockMediatr;
+
+        public Given_A_PensionsRegulatorController()
+        {
+            MockMediatr
+                =
+                Substitute.For<IMediator>();
+
+            SUT
+                =
+                new PensionsRegulatorController(
+                    MockMediatr,
+                    Substitute.For<ILogger<PensionsRegulatorController>>());
+        }
+
+        [ExcludeFromCodeCoverage]
+        public class When_Organisations_Are_Request_By_Paye_And_AORN
+            : Given_A_PensionsRegulatorController
+        {
+            private string _payeRef = "payes";
+            private string _aorn = "aorn";
+
+            [SetUp]
+            public async Task When()
+            {
+                await 
+                SUT
+                    .Get(
+                        _payeRef,
+                        _aorn);
+            }
+
+            [Test]
+            public void Then_Data_Is_Retrieved_Using_Both_Paye_And_AORN()
+            {
+                MockMediatr
+                    .Received()
+                    .Send(
+                        Arg.Is<GetValidatedOrganisations>(
+                            arg => arg.PAYEReference.Equals(
+                                       _payeRef,
+                                       StringComparison.Ordinal)
+                                   && arg.AccountOfficeReferenceNumber.Equals(
+                                       _aorn,
+                                       StringComparison.Ordinal)));
+            }
+
+            [Test]
+            public void Then_Date_Is_Not_Retrieved_Using_Paye_Only()
+            {
+                MockMediatr.DidNotReceive()
+                    .Send(Arg.Any<GetOrganisations>());
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.PensionsRegulatorApi.UnitTests/Controllers/PayeOnly/Given_A_PensionsRegulatorController/Given_A_PensionsRegulatorController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NUnit.Framework;
+using PensionsRegulatorApi.Application.Queries;
+using PensionsRegulatorApi.Controllers;
+
+namespace SFA.DAS.PensionsRegulatorApi.UnitTests.Controllers.PayeOnly.Given_A_PensionsRegulatorController
+{
+    [ExcludeFromCodeCoverage]
+    public class Given_A_PensionsRegulatorController
+    {
+        protected PensionsRegulatorController SUT;
+        protected IMediator MockMediatr;
+
+        public Given_A_PensionsRegulatorController()
+        {
+            MockMediatr
+                =
+                Substitute.For<IMediator>();
+
+            SUT
+                =
+                new PensionsRegulatorController(
+                    MockMediatr,
+                    Substitute.For<ILogger<PensionsRegulatorController>>());
+        }
+
+        [ExcludeFromCodeCoverage]
+        public class When_Organisations_Are_Request_By_Paye_Only
+            : Given_A_PensionsRegulatorController
+        {
+            private string _payeRef = "payes";
+
+            [SetUp]
+            public async Task When()
+            {
+                await 
+                SUT
+                    .Get(
+                        _payeRef);
+            }
+
+            [Test]
+            public void Then_Data_Is_Retrieved_Using_Paye_Only()
+            {
+                MockMediatr
+                    .Received()
+                    .Send(
+                        Arg.Is<GetOrganisations>(
+                            arg => arg.PAYEReference.Equals(
+                                       _payeRef,
+                                       StringComparison.Ordinal)));
+            }
+
+            [Test]
+            public void Then_Data_Is_Not_Retrieved_Using_Paye_And_AORN()
+            {
+                MockMediatr
+                    .DidNotReceive()
+                    .Send(
+                        Arg.Any<GetValidatedOrganisations>());
+            }
+        }
+    }
+}
