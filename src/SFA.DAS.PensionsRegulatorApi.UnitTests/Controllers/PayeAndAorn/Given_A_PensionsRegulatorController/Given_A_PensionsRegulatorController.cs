dba0d7f3e86d7cfa3481058c9a340e719d66e1a3
Neil Winstanley <Winsto@users.noreply.github.com>
2019-06-14 10:49:23 +0100

    AML-3692: remove redundant using statement.

--- src/SFA.DAS.PensionsRegulatorApi.UnitTests/Controllers/PayeAndAorn/Given_A_PensionsRegulatorController/Given_A_PensionsRegulatorController.cs
+++ src/SFA.DAS.PensionsRegulatorApi.UnitTests/Controllers/PayeAndAorn/Given_A_PensionsRegulatorController/Given_A_PensionsRegulatorController.cs
@@ -3,7 +3,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Bson;
 using NSubstitute;
 using NUnit.Framework;
 using PensionsRegulatorApi.Application.Queries;
