5a1d542cb31f998ad486e8afd913c45dfcdb6a16
Neil Winstanley <Winsto@users.noreply.github.com>
2019-06-14 13:44:06 +0100

    AML-3692: modify sprocs to call newly added view.

--- src/SFA.DAS.PensionsRegulator.Database/StoredProcedures/GetOrganisationsByPAYEReference.sql
+++ src/SFA.DAS.PensionsRegulator.Database/StoredProcedures/GetOrganisationsByPAYEReference.sql
@@ -2,17 +2,15 @@
 	@PAYESchemeReference varchar(25)
 AS
 SELECT 
-Organisation.OrganisationName as Name,
-Organisation.Status as Status,
-Organisation.TPR_Unique_Id as UniqueIdentity,
-OrganisationAddress.AddressLine1 as AddressLine1,
-OrganisationAddress.AddressLine2 as AddressLine2,
-OrganisationAddress.AddressLine3 as AddressLine3,
-OrganisationAddress.AddressLine4 as AddressLine4,
-OrganisationAddress.AddressLine5 as AddressLine5,
-OrganisationAddress.PostCode as PostCode
+Name,
+Status,
+UniqueIdentity,
+AddressLine1,
+AddressLine2,
+AddressLine3,
+AddressLine4,
+AddressLine5,
+PostCode
 FROM
-Organisation JOIN
-OrganisationPAYEScheme on OrganisationPAYEScheme.Employer_SK = Organisation.Employer_SK LEFT JOIN
-OrganisationAddress on OrganisationAddress.Employer_SK = OrganisationPAYEScheme.Employer_SK 
-WHERE OrganisationPAYEScheme.PAYESchemeRef = @PAYESchemeReference
+OrganisationsWithAddressesAndReferences
+WHERE PAYERef = @PAYESchemeReference
