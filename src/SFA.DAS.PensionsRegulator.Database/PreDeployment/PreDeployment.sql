60df4c317c820e0a6656c3f380214dbaae9b5488
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2019-10-01 15:43:33 +0100

    DeleteExistingOrgTables

---
+++ src/SFA.DAS.PensionsRegulator.Database/PreDeployment/PreDeployment.sql
@@ -0,0 +1,20 @@
+﻿/* Drop Existing Tables and Re-Build */
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_NAME = N'Organisation'
+		      AND TABLE_SCHEMA=N'dbo'
+	      )
+DROP TABLE dbo.Organisation
+
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_NAME = N'OrganisationAddress'
+		      AND TABLE_SCHEMA=N'dbo'
+	      )
+DROP TABLE dbo.OrganisationAddress
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_NAME = N'OrganisationPAYEScheme'
+		      AND TABLE_SCHEMA=N'dbo'
+	      )
+DROP TABLE dbo.OrganisationPAYEScheme
