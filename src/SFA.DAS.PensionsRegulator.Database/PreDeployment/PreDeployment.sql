ef71ee8e065347b12d03d74e0c1bf5a04cfa8d38
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2019-10-02 12:28:14 +0100

    RemoveStoredProcsThatReferenceOldTables

--- src/SFA.DAS.PensionsRegulator.Database/PreDeployment/PreDeployment.sql
+++ src/SFA.DAS.PensionsRegulator.Database/PreDeployment/PreDeployment.sql
@@ -1,4 +1,16 @@
-﻿
+﻿IF EXISTS (select * from INFORMATION_SCHEMA.ROUTINES
+            where ROUTINE_NAME='uSP_LoadTPRFile'
+              and ROUTINE_SCHEMA='dbo'
+		  )
+DROP PROCEDURE dbo.uSP_LoadTPRFile
+
+
+IF EXISTS (select * from INFORMATION_SCHEMA.ROUTINES
+            where ROUTINE_NAME='uSP_LoadTargetTables'
+              and ROUTINE_SCHEMA='dbo'
+		  )
+DROP PROCEDURE dbo.uSP_LoadTargetTables
+
 
 
 
