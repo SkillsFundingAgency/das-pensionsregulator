9b7663aca58f359a07047b2ec180f0daa7b66f4c
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2019-10-02 11:05:58 +0100

    DeleteStrategicStoredProcToResolveTestDeploymentIssue

--- src/SFA.DAS.PensionsRegulator.Database/PreDeployment/PreDeployment.sql
+++ src/SFA.DAS.PensionsRegulator.Database/PreDeployment/PreDeployment.sql
@@ -1,6 +1,12 @@
 ﻿/* Drop Existing Tables and Re-Build */
 
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_NAME = N'OrganisationPAYEScheme'
+		      AND TABLE_SCHEMA=N'dbo'
+	      )
+DROP TABLE dbo.OrganisationPAYEScheme
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
             WHERE TABLE_NAME = N'Organisation'
 		      AND TABLE_SCHEMA=N'dbo'
 	      )
@@ -13,8 +19,22 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
 	      )
 DROP TABLE dbo.OrganisationAddress
 
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
-            WHERE TABLE_NAME = N'OrganisationPAYEScheme'
-		      AND TABLE_SCHEMA=N'dbo'
-	      )
-DROP TABLE dbo.OrganisationPAYEScheme
+IF EXISTS (select * from INFORMATION_SCHEMA.ROUTINES
+            where ROUTINE_NAME='uSP_LoadTPRFile'
+              and ROUTINE_SCHEMA='dbo'
+		  )
+DROP PROCEDURE dbo.uSP_LoadTPRFile
+
+
+IF EXISTS (select * from INFORMATION_SCHEMA.ROUTINES
+            where ROUTINE_NAME='uSP_LoadTargetTables'
+              and ROUTINE_SCHEMA='dbo'
+		  )
+DROP PROCEDURE dbo.uSP_LoadTargetTables
+
+
+
+
+
+
+
