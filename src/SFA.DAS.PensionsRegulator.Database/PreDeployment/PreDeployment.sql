15fb1178f209c89db3671c56495a54646e9b31f1
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2019-10-11 13:07:34 +0100

    Change Drop Index syntax

--- src/SFA.DAS.PensionsRegulator.Database/PreDeployment/PreDeployment.sql
+++ src/SFA.DAS.PensionsRegulator.Database/PreDeployment/PreDeployment.sql
@@ -6,28 +6,35 @@ SET @Date=getdate()
 IF @Date < '2019-10-30'
 BEGIN
 
-IF  EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'CI_TPR_UniqueKey' AND object_id = OBJECT_ID('Stg_Tpr_1218')) 
+IF  EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'CI_TPR_UniqueKey' AND object_id = OBJECT_ID('dbo.Stg_Tpr_1218')) 
+BEGIN
 DROP INDEX [CI_TPR_UniqueKey] ON dbo.Stg_Tpr_1218
+END
 
 
-
-IF  EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'NCI_TPR_AORN' AND object_id = OBJECT_ID('Stg_Tpr_1218')) 
+IF  EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'NCI_TPR_AORN' AND object_id = OBJECT_ID('dbo.Stg_Tpr_1218')) 
+BEGIN
 DROP INDEX [NCI_TPR_AORN] ON dbo.Stg_Tpr_1218
+END
 
 
-
-IF  EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'NCI_TPR_PAYE' AND object_id = OBJECT_ID('Stg_Tpr_1218')) 
+IF  EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'NCI_TPR_PAYE' AND object_id = OBJECT_ID('dbo.Stg_Tpr_1218')) 
+BEGIN
 DROP INDEX [NCI_TPR_PAYE] ON dbo.Stg_Tpr_1218
+END
 
 
 
-IF  EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'NCI_TPR_PAYE_AORN' AND object_id = OBJECT_ID('Stg_Tpr_1218')) 
+IF  EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'NCI_TPR_PAYE_AORN' AND object_id = OBJECT_ID('dbo.Stg_Tpr_1218')) 
+BEGIN
 DROP INDEX [NCI_TPR_PAYE_AORN] ON dbo.Stg_Tpr_1218
+END
 
 
-
-IF  EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'NCI_TPR_PAYE_DERIVED' AND object_id = OBJECT_ID('Stg_Tpr_1218')) 
+IF  EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'NCI_TPR_PAYE_DERIVED' AND object_id = OBJECT_ID('dbo.Stg_Tpr_1218')) 
+BEGIN
 DROP INDEX [NCI_TPR_PAYE_DERIVED] ON dbo.Stg_Tpr_1218
+END
 
 END
 
