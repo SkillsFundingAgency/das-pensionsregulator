a3cf7af0205d51cfafdb15db2a367ad9f818d4fb
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-01-20 17:36:40 +0000

    SwitchTPRViewToPointToStrategicTables,IncreaseFunctionTimeOut

--- src/SFA.DAS.PensionsRegulator.Database/Views/OrgAornPayeDetails.sql
+++ src/SFA.DAS.PensionsRegulator.Database/Views/OrgAornPayeDetails.sql
@@ -2,26 +2,24 @@
 
 CREATE VIEW [dbo].[OrgAornPayeDetails]
 AS
-SELECT
- LTRIM(RTRIM(Name1))+' '+LTRIM(RTRIM(Name2)) as OrganisationName
-,CASE WHEN EndDateCode=0 THEN 'Not Closed' 
-      WHEN EndDateCode=1 THEN 'Transfer Out' 
-	  WHEN EndDateCode=2 THEN 'Merger Out'
-	  WHEN EndDateCode=3 THEN 'Succession Out'
-	  WHEN EndDateCode=4 THEN 'Ceased'
-	  WHEN EndDateCode=5 THEN 'Cancelled'
-	  ELSE 'Unknown'
-  END                                        as OrganisationStatus
-, PayeDerived                                as PayeRef
-, AORN                                       as AORN
-, UniqueID                                   as TPRUniqueKey
-, LTRIM(RTRIM(AddressLine1))                 as AddressLine1
-, LTRIM(RTRIM(AddressLine2))                 as AddressLine2
-, LTRIM(RTRIM(AddressLine3))                 as AddressLine3
-, LTRIM(RTRIM(AddressLine4))                 as AddressLine4
-, LTRIM(RTRIM(AddressLine5))                 as AddressLine5
-, LTRIM(RTRIM(PostCode))                     as PostCode
-FROM [dbo].Stg_Tpr_1218
+   SELECT 
+          Org.OrganisationName              as OrganisationName
+         ,OrgPAYE.SchemeEndDateCodeDesc     as OrganisationStatus
+         ,OrgPAYE.PAYEScheme                as PayeRef
+         ,Org.AOReference                   as AORN
+         ,Org.TPRUniqueId                   as TPRUniqueKey
+         ,OrgAdd.AddressLine1               as AddressLine1
+         ,OrgAdd.AddressLine2               as AddressLine2
+         ,OrgAdd.AddressLine3               as AddressLine3
+         ,OrgAdd.AddressLine4               as AddressLine4
+         ,OrgAdd.AddressLine5               as AddressLine5
+         ,OrgAdd.PostCode                   as PostCode
+     FROM
+          Tpr.Organisation Org
+	 JOIN Tpr.OrganisationPAYEScheme OrgPAYE
+	   on OrgPAYE.OrgSK = Org.OrgSK 
+     JOIN Tpr.OrganisationAddress OrgAdd
+       on OrgAdd.OrgSK = Org.OrgSK 
 GO
 
 
