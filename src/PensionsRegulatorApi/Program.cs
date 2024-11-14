094e4062fef6ae30ad3f9a879544290f6e30a002
Jon Harris <jon.harris@fuitsu.com>
2024-11-14 09:11:39 +0000

    Removing NLog

--- src/PensionsRegulatorApi/Program.cs
+++ src/PensionsRegulatorApi/Program.cs
@@ -1,34 +1,13 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using NLog;
-using NLog.Web;
 
 namespace PensionsRegulatorApi;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        var logger = LogManager.LogFactory.Setup().LoadConfigurationFromFile("nlog.config").GetCurrentClassLogger();
-
-        try
-        {
-            logger.Info("Starting up host");
-            CreateWebHostBuilder(args).Build().Run();
-        }
-        catch (Exception ex)
-        {
-            logger.Fatal(ex, "Stopped program because of exception");
-            throw;
-        }
-        finally
-        {
-            LogManager.Shutdown();
-        }
-    }
+    public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
 
     private static IHostBuilder CreateWebHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
-            .UseNLog()
             .ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>());
 }
